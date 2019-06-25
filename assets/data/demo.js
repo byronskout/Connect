9eaa3d9550dda961783ab4c17a2e6e853d2da200
github username <vyskoutaris@hotmail.com>
2019-06-25 22:27:56 +0100

    User data added

---
+++ assets/data/demo.js
@@ -0,0 +1,115 @@
+module.exports = [
+	{
+		id: 1,
+		name: 'Bob Wilson',
+		status: 'Online',
+		match: '78',
+		description:
+			'Full-time Traveller. Photographer. Dancing. Surfing',
+		message:
+			'After all, tomorrow is another day',
+		image: require('../images/01.jpg')
+	},
+	{
+		id: 2,
+		name: 'Leah Davis',
+		match: '93',
+		description:
+			'Enjoys Travelling. Full-time Painter. Photographer. Part Time Singer.',
+		status: 'Offline',
+		message: "I' ll have what she's having",
+		image: require('../images/02.jpg')
+	},
+	{
+		id: 3,
+		name: 'Erik Brown',
+		match: '45',
+		description:
+			'Full-time Baker. Digital Arts. Loves Jigsaw Puzzles. Learning How to Dance.',
+		status: 'Offline',
+		message:
+			"Forget it, Jake. It's Chinatown",
+		image: require('../images/03.jpg')
+	},
+	{
+		id: 4,
+		name: 'Juana Jones',
+		match: '88',
+		description:
+			'Book collector. Meditates. Learning to Code. Part Time Dancer.',
+		status: 'Online',
+		message: "Bats frighten me. It's time my enemies shared my dread.",
+		image: require('../images/04.jpg')
+	},
+	{
+		id: 5,
+		name: 'Nicoletta Thompson',
+		match: '76',
+		description:
+			'Camping. Coin collector. Photographer. Attending Gallery Events.',
+		status: 'Offline',
+		message: "It's not who I am underneath but what I do that defines me.",
+		image: require('../images/05.jpg')
+	},
+	{
+		id: 6,
+		name: 'Fedora Collins',
+		match: '95',
+		description:
+			'Dancer. Antique Collector. Loves Fishing. Foodie.',
+		status: 'Online',
+		message:
+			'You have nothing, nothing to threaten me with. Nothing to do with all your strength.',
+		image: require('../images/06.jpg')
+	},
+	{
+		id: 7,
+		name: 'Maxine Blanc',
+		match: '67',
+		description:
+			'Enjoys Travelling. Globe Trotter. Photographer.',
+		status: 'Online',
+		message:
+			"I'm just one stomach flu away from my goal weight",
+		image: require('../images/07.jpg')
+	},
+	{
+		id: 8,
+		name: 'Byron Skoutaris',
+		match: '85',
+		description:
+			'Full-time Coder. Loves Travelling. Beginner Photographer. Part time Painter.',
+		age: '27',
+		location: 'London, GB',
+		infoa: '5"10',
+		infob: 'Coffee Totaller, Cinema & Travel',
+		infoc: 'Beaches, Camping, Movies',
+		infod: 'Last seen: 23h ago',
+		status: 'Online',
+		message:
+			"Made it, Ma! Top of the world",
+		image: require('../images/08.png')
+	},
+	{
+		id: 9,
+		name: 'Erna Williams',
+		match: '74',
+		description:
+			'Book collector. Meditates. Learning to Code. Part Time Dancer.',
+		status: 'Online',
+		message:
+			'All good things come to an end. They call it a Royale with cheese',
+		image: require('../images/09.jpg')
+	},
+	{
+		id: 10,
+		name: 'Norman Evans',
+		match: '98',
+		description:
+			'Full-time Dancer. Arts. Loves Cinema. Learning New Languages.',
+		status: 'Offline',
+		message:
+			"I'm going to make him an offer he can't refuse",
+		image: require('../images/10.jpg')
+	}
+];
