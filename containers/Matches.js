17809ab2f7eb722cbcc4b565e7fe37e7ab3d1f38
github username <vyskoutaris@hotmail.com>
2019-06-25 19:07:42 +0100

    Matches.js added

---
+++ containers/Matches.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import {
+	ScrollView,
+	View,
+	Text,
+	TouchableOpacity,
+	ImageBackground,
+	FlatList
+} from "react-native";
+
+const Matches = () => {
+	return (
+
+			<View >
+				<ScrollView>
+					<View>
+						<Text>Matches</Text>
+						<TouchableOpacity>
+							<Text>
+							</Text>
+						</TouchableOpacity>
+					</View>
+
+
+							<TouchableOpacity>
+							</TouchableOpacity>
+				</ScrollView>
+			</View>
+	);
+};
+
+export default Matches;
