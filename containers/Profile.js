3979db79e3a2da74aa0b869cb7c220801fb3845b
github username <vyskoutaris@hotmail.com>
2019-06-29 10:58:58 +0100

    Profile updated

--- containers/Profile.js
+++ containers/Profile.js
@@ -7,47 +7,65 @@ import {
 	ImageBackground,
 	TouchableOpacity
 } from "react-native";
-
+import ProfileItem from "../components/ProfileItem";
+import Icon from "../components/Icon";
+import Demo from "../assets/data/demo.js";
 
 const Profile = () => {
 	const {
 		age,
 		image,
-		infoa,
-		infob,
-		infoc,
-		infod,
+		info1,
+		info2,
+		info3,
+		info4,
 		location,
 		match,
 		name
-	}
+	} = Demo[7];
 
 	return (
-		<ImageBackground>
-			<ScrollView >
+		<ImageBackground
+			source={require("../assets/images/bg.png")}
+		>
+			<ScrollView>
 				<ImageBackground source={image}>
 					<View>
 						<TouchableOpacity>
-							<Text >
+							<Text>
+								<Icon name="chevronLeft" />
 							</Text>
 						</TouchableOpacity>
 
 						<TouchableOpacity>
 							<Text>
+								<Icon name="optionsV" />
 							</Text>
 						</TouchableOpacity>
 					</View>
 				</ImageBackground>
 
+				<ProfileItem
+					matches={match}
+					name={name}
+					age={age}
+					location={location}
+					info1={info1}
+					info2={info2}
+					info3={info3}
+					info4={info4}
+				/>
 
 				<View>
 					<TouchableOpacity>
 						<Text>
+							<Icon name="optionsH" />
 						</Text>
 					</TouchableOpacity>
 
 					<TouchableOpacity>
-						<Text >
+						<Text>
+							<Icon name="chat" />
 						</Text>
 						<Text>Start chatting</Text>
 					</TouchableOpacity>
