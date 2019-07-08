9a0b68a3a1510bbd2a4782694d4ff211aa2c0fa1
byronskout <vyskoutaris@hotmail.com>
2019-07-08 14:32:16 +0100

    ProfileItem added features

--- components/ProfileItem.js
+++ components/ProfileItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Text, View } from "react-native";
+import Icon from "./Icon";
 
 const ProfileItem = ({
 	age,
@@ -13,13 +14,47 @@ const ProfileItem = ({
 	name
 }) => {
 	return (
+		<View>
 			<View>
-				<Text >
-					 Match!
+				<Text>
+					<Icon name="heart" /> {matches}% Match!
 				</Text>
 			</View>
 
+			<Text>{name}</Text>
 
+			<Text>
+				{age} - {location}
+			</Text>
+
+			<View>
+				<Text>
+					<Icon name="user" />
+				</Text>
+				<Text>{info1}</Text>
+			</View>
+
+			<View>
+				<Text>
+					<Icon name="circle" />
+				</Text>
+				<Text>{info2}</Text>
+			</View>
+
+			<View>
+				<Text>
+					<Icon name="hashtag" />
+				</Text>
+				<Text>{info3}</Text>
+			</View>
+
+			<View>
+				<Text>
+					<Icon name="calendar" />
+				</Text>
+				<Text>{info4}</Text>
+			</View>
+		</View>
 	);
 };
 
